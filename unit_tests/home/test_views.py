0997af54e233e41d2a248f12a5681063ff0f2aab
Théo FERNANDO <theo.fernando@isen.yncrea.fr>
2025-04-25 11:22:17 +0200

    test: unit test for the price filtering

--- unit_tests/home/test_views.py
+++ unit_tests/home/test_views.py
@@ -4,6 +4,7 @@ from pytest_django.asserts import assertTemplateUsed
 import pytest
 
 from home.views import RedirectHomeView, HomeView
+from products.models import Product
 
 
 def test_RedirectHomeView():
@@ -30,3 +31,26 @@ def test_HomeView():
     
     assert response.status_code == 200
     assertTemplateUsed(response, 'home.html')
+
+@pytest.mark.django_db
+def test_HomeView_sorting():
+    client = Client()
+
+    # Création de produits pour le test
+    Product.objects.create(name="Produit A", price=10.0)
+    Product.objects.create(name="Produit B", price=20.0)
+    Product.objects.create(name="Produit C", price=15.0)
+
+    # Test tri par prix croissant
+    response_asc = client.get(reverse('home') + '?sort=asc')
+    assert response_asc.status_code == 200
+    assertTemplateUsed(response_asc, 'home.html')
+    products_asc = list(response_asc.context['object_list'])
+    assert [product.price for product in products_asc] == [10.0, 15.0, 20.0]
+
+    # Test tri par prix décroissant
+    response_desc = client.get(reverse('home') + '?sort=desc')
+    assert response_desc.status_code == 200
+    assertTemplateUsed(response_desc, 'home.html')
+    products_desc = list(response_desc.context['object_list'])
+    assert [product.price for product in products_desc] == [20.0, 15.0, 10.0]
